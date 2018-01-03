3d81db0aaf5ede3b6c16d22ef314b969bd4747e2
Harsh Desai <harsh@portworx.com>
2018-01-03 13:17:47 -0800

    add mysql get pods command

--- deploy-px-mysql/background.sh
+++ deploy-px-mysql/background.sh
@@ -3,4 +3,6 @@ ssh root@[[HOST_IP]] 'echo "Host *" >> /root/.ssh/config && echo "    StrictHost
 ssh root@node01 'docker pull portworx/px-enterprise:1.2.12.0'
 ssh root@node02 'docker pull portworx/px-enterprise:1.2.12.0'
 ssh root@node03 'docker pull portworx/px-enterprise:1.2.12.0'
-
+ssh root@node01 'docker pull mysql:5.6'
+ssh root@node02 'docker pull mysql:5.6'
+ssh root@node03 'docker pull mysql:5.6'

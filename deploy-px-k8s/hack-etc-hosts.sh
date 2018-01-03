5c228ad4b820177900ac0aff7072eb248cebbcdc
Harsh Desai <harsh@portworx.com>
2018-01-02 17:46:15 -0800

    attempt hack etcd script

--- deploy-px-k8s/hack-etc-hosts.sh
+++ deploy-px-k8s/hack-etc-hosts.sh
@@ -1,5 +1,3 @@
-#!/bin/bash -e
-
 ssh root@node01 "sed -i.bkp 's/127.0.0.1 node01/#127.0.0.1 node01/g' /etc/hosts"
 ssh root@node02 "sed -i.bkp 's/127.0.0.1 node01/#127.0.0.1 node01/g' /etc/hosts"
 ssh root@node03 "sed -i.bkp 's/127.0.0.1 node01/#127.0.0.1 node01/g' /etc/hosts"

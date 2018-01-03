4909ca5e570d0212daeb1f721c242199eee35d4c
Harsh Desai <harsh@portworx.com>
2018-01-03 11:48:16 -0800

    remove etcd hosts hack

--- deploy-px-k8s/hack-etc-hosts.sh
+++
@@ -1,3 +0,0 @@
-ssh root@node01 "sed -i.bkp 's/127.0.0.1 node01/#127.0.0.1 node01/g' /etc/hosts"
-ssh root@node02 "sed -i.bkp 's/127.0.0.1 node01/#127.0.0.1 node01/g' /etc/hosts"
-ssh root@node03 "sed -i.bkp 's/127.0.0.1 node01/#127.0.0.1 node01/g' /etc/hosts"

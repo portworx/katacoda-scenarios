e06fe014d64233cc46ff876d9601e180c2037484
Francois Martel <fmrtl@fm.fios-router.home>
2018-02-12 12:20:25 -0500

    while loop unecessary

--- px-k8s-postgres/background.sh
+++ px-k8s-postgres/background.sh
@@ -3,16 +3,3 @@ ssh root@[[HOST_IP]] 'echo "Host *" >> /root/.ssh/config && echo "    StrictHost
 ssh root@node01 'docker pull portworx/px-enterprise:1.2.12.0'
 ssh root@node02 'docker pull portworx/px-enterprise:1.2.12.0'
 ssh root@node03 'docker pull portworx/px-enterprise:1.2.12.0'
-#!/bin/bash -e
-
-while true; do
-    NUM_READY=`kubectl get nodes 2> /dev/null | grep -v NAME | awk '{print $2}' | grep -e ^Ready | wc -l`
-    if [ "${NUM_READY}" == "4" ]; then
-        echo "All ${NUM_READY} Kubernetes nodes are ready !"
-        break
-    else
-        echo "Waiting for all Kubernetes nodes to be ready. Current ready nodes: ${NUM_READY}"
-        kubectl get nodes
-    fi
-    sleep 5
-done

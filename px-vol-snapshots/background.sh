220c15cd48c8d60d0e550eb62a99c1f4798e6645
Francois Martel <fmrtl@fm.fios-router.home>
2018-02-16 08:12:00 -0500

    update

--- px-vol-snapshots/background.sh
+++ px-vol-snapshots/background.sh
@@ -3,4 +3,3 @@ ssh root@[[HOST_IP]] 'echo "Host *" >> /root/.ssh/config && echo "    StrictHost
 ssh root@node01 'docker pull portworx/px-enterprise:1.2.12.0'
 ssh root@node02 'docker pull portworx/px-enterprise:1.2.12.0'
 ssh root@node03 'docker pull portworx/px-enterprise:1.2.12.0'
-./ingress.sh >> ingress.yaml

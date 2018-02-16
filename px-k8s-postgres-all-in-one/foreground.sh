5cf8cae828b9647986a82e233434168a3c6b3f09
Francois Martel <fmrtl@fm.fios-router.home>
2018-02-16 07:36:39 -0500

    update

--- px-k8s-postgres-all-in-one/foreground.sh
+++ px-k8s-postgres-all-in-one/foreground.sh
@@ -1,6 +1,6 @@
 launch.sh
 echo "sytem coming online, this will take 20 seconds"
-wait 15
+sleep 15
 ssh-keyscan node01 >> ~/.ssh/known_hosts
 ssh-keyscan node02 >> ~/.ssh/known_hosts
 ssh-keyscan node03 >> ~/.ssh/known_hosts

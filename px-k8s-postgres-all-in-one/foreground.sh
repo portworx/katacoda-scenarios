6eb7a4e6cbbae397d96ba8d34835795f40d4d7a4
Francois Martel <fmrtl@fm.fios-router.home>
2018-02-16 08:22:14 -0500

    update

--- px-k8s-postgres-all-in-one/foreground.sh
+++ px-k8s-postgres-all-in-one/foreground.sh
@@ -1,7 +1,1 @@
-echo "This initialization will only take a minute, once the screen clears you're ready to start."
 launch.sh
-sleep 15
-ssh-keyscan node01 >> ~/.ssh/known_hosts
-ssh-keyscan node02 >> ~/.ssh/known_hosts
-ssh-keyscan node03 >> ~/.ssh/known_hosts
-clear

6686db8ac90fbc7309e9b0ac68e515084d9f7bd1
Francois Martel <fmrtl@fm.fios-router.home>
2018-02-16 07:15:04 -0500

    security fast track

--- px-k8s-postgres-all-in-one/background.sh
+++ px-k8s-postgres-all-in-one/background.sh
@@ -1,4 +1,6 @@
-ssh root@[[HOST_IP]] 'echo "Host *" >> /root/.ssh/config && echo "    StrictHostKeyChecking no" >> /root/.ssh/config && chmod 400 /root/.ssh/config'
+ssh-keyscan node01 >> ~/.ssh/known_hosts
+ssh-keyscan node02 >> ~/.ssh/known_hosts
+ssh-keyscan node03 >> ~/.ssh/known_hosts
 
 ssh root@node01 'docker pull portworx/px-enterprise:1.2.12.0'
 ssh root@node02 'docker pull portworx/px-enterprise:1.2.12.0'

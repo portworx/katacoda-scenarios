87987e70524a85a05b322600835ce808c55e112c
Francois Martel <fmrtl73@gmail.com>
2018-02-16 10:19:11 -0500

    update

--- px-k8s-postgres-all-in-one/background.sh
+++ px-k8s-postgres-all-in-one/background.sh
@@ -1,7 +1,3 @@
 ssh root@[[HOST_IP]] 'echo "Host *" >> /root/.ssh/config && echo "    StrictHostKeyChecking no" >> /root/.ssh/config && chmod 400 /root/.ssh/config'
-
-ssh root@node01 'docker pull portworx/px-enterprise:1.2.12.0'
-ssh root@node02 'docker pull portworx/px-enterprise:1.2.12.0'
-ssh root@node03 'docker pull portworx/px-enterprise:1.2.12.0'
 kubectl create -f stork.yaml
 kubectl create -f stork-scheduler.yaml

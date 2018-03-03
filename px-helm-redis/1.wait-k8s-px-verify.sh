1a698d6286cac92a703fc8d19f8fd484de880fbb
Francois Martel <fmrtl73@gmail.com>
2018-03-03 09:08:10 -0500

    iterate

--- px-helm-redis/1.wait-k8s-px-verify.sh
+++ px-helm-redis/1.wait-k8s-px-verify.sh
@@ -1,1 +1,1 @@
-[ `kubectl get pods -n kube-system 2>/dev/null | grep portworx | awk '{print $2}' | grep -e 0/1 | wc -l` -eq '0' ] && echo "done"
+[ `kubectl get pods -n kube-system 2>/dev/null | grep portworx | awk '{print $2}' | grep -e 0/1 | wc -l` -eq '0' ] && echo \"done\"

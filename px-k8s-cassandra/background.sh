ed63205f4930106970cfe98f5d5dfaf9896c3a4a
Francois Martel <fmrtl73@gmail.com>
2018-03-07 08:15:04 -0500

    Revert "Revert "push origin master""
    This reverts commit 8f142ded3fb5a0a4470f76bebbf5cfa5335445c8.

--- px-k8s-cassandra/background.sh
+++
@@ -1,3 +0,0 @@
-ssh root@[[HOST_IP]] 'echo "Host *" >> /root/.ssh/config && echo "    StrictHostKeyChecking no" >> /root/.ssh/config && chmod 400 /root/.ssh/config'
-kubectl create -f stork.yaml
-kubectl create -f stork-scheduler.yaml

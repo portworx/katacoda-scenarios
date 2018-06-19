916434e23a1fefc5df9334dfd88fe3215176bb24
Ben Hall <ben@benhall.me.uk>
2018-06-19 18:20:23 +0100

    Updates based on feedback from Joe

---
+++ links.md
@@ -0,0 +1,1 @@
+More information at https://docs.portworx.com/
---
+++ playground/background.sh
@@ -0,0 +1,3 @@
+ssh root@[[HOST_IP]] 'echo "Host *" >> /root/.ssh/config && echo "    StrictHostKeyChecking no" >> /root/.ssh/config && chmod 400 /root/.ssh/config'
+
+

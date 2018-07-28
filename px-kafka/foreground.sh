571418cfa61dd2a09c66bbdbecfe9d8f57772c0e
Francois Martel <fmrtl73@gmail.com>
2018-07-28 13:48:15 -0400

    i++

--- px-kafka/foreground.sh
+++ px-kafka/foreground.sh
@@ -1,3 +1,3 @@
 launch.sh
-git clone http://github.com/fmrtl73/katacoda-scenarios-1 > out.log
+git clone http://github.com/fmrtl73/katacoda-scenarios-1 >> out.log
 cp katacoda-scenarios-1/px-kafka/assets/* .

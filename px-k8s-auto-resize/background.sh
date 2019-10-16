28f6144467bd219a2730719330aef3fad1217b38
wallnerryan <ryan.wallner@portworx.com>
2019-10-16 15:52:02 -0400

    clean up script

--- px-k8s-auto-resize/background.sh
+++ px-k8s-auto-resize/background.sh
@@ -1,14 +1,6 @@
 ssh root@[[HOST_IP]] 'echo "Host *" >> /root/.ssh/config && echo "    StrictHostKeyChecking no" >> /root/.ssh/config && chmod 400 /root/.ssh/config'
-
 kubectl create -f portwox-pxc-operator.yaml
-
-until kubectl get po -n kube-system -l k8s-app=prometheus-operator | grep Running
-do 
-	echo "waiting for prometheus operator"
-done
-
+until kubectl get po -n kube-system -l k8s-app=prometheus-operator | grep "Running"; do echo "waiting for prometheus operator"; done
 kubectl create -f pwx-monitoring.yaml
-
 kubectl create -f autopilot-cfg.yaml
-
 kubectl create -f autopilot.yaml

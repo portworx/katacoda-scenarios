5e59dd65461564721d9032076cf8ca5ed1397108
wallnerryan <ryan.wallner@portworx.com>
2019-10-21 09:44:27 -0400

    make things flow better as prometheus  wasnt always ready and this can make the Rule not work if someone clicks to fast

--- px-k8s-auto-resize/1.verify.sh
+++ px-k8s-auto-resize/1.verify.sh
@@ -1,1 +1,1 @@
-(( $(kubectl get pods -n kube-system -l name=autopilot 2>/dev/null | grep Running | grep 1/1 | awk '{print $2}' | wc -l) == 1)) && echo \"done\"
+(( $(kubectl get pods -n kube-system -l statefulset.kubernetes.io/pod-name=prometheus-prometheus-0 2>/dev/null | grep Running | grep 3/3 | awk '{print $2}' | wc -l) == 1)) && echo \"done\"

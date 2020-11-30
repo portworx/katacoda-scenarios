1be1f4e075c9291784e088318846cd0f134f3a6d
wallnerryan <rwallner@purestorage.com>
2020-11-30 13:36:18 -0500

    update to 1.13

--- px-mysql/10.verify.sh
+++ px-mysql/10.verify.sh
@@ -1,1 +1,1 @@
-ssh root@host01 "[ \`kubectl get pods -l app=mysql 2>/dev/null | grep Running | grep 1/1 | awk '{print $2}' | wc -l\` -eq '1' ] && echo \"done\""
+(( $(kubectl get pods -l app=mysql 2>/dev/null | grep Running | grep 1/1 | awk '{print $2}' | wc -l) == 1)) && echo \"done\"

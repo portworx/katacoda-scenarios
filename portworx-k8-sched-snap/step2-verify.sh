40d2959011b4376d68ea9ca2870f499cffc69000
Ryan Wallner <wallnerryan@users.noreply.github.com>
2020-12-08 14:37:44 -0500

    fix verify

--- portworx-k8-sched-snap/step2-verify.sh
+++ portworx-k8-sched-snap/step2-verify.sh
@@ -1,4 +1,1 @@
-if [[ `kubectl get  schedulepolicy  daily-schedule -n mysql-app -o json  | jq -r ".policy.daily.time" | grep -w 10:00PM` ]] && [[ `kubectl get  schedulepolicy  daily-schedule -n mysql-app -o json  | jq -r ".policy.daily.retain" | grep -w 5` ]]
-then
-	echo "done"
-fi
+if [[ `kubectl get  schedulepolicy  daily-schedule -o json  | jq -r ".policy.daily.time" | grep -w 10:00PM` ]] && [[ `kubectl get  schedulepolicy  daily-schedule -o json  | jq -r ".policy.daily.retain" | grep -w 5` ]]; then echo"done"; fi

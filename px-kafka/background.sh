025359b9365e473230aa673406f2ecce56d20f8a
bhavin04890 <bshah@purestorage.com>
2021-09-23 13:37:54 -0400

    Update background.sh

--- px-kafka/background.sh
+++ px-kafka/background.sh
@@ -1,5 +1,1 @@
 ssh root@[[HOST_IP]] 'echo "Host *" >> /root/.ssh/config && echo "    StrictHostKeyChecking no" >> /root/.ssh/config && chmod 400 /root/.ssh/config'
-git clone http://github.com/fmrtl73/katacoda-scenarios-1
-cp katacoda-scenarios-1/px-kafka/assets/* .
-
-./ingress.sh >> ingress.yaml
